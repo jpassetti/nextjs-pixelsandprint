e56c76fc791eaf0d3c1c89444594dab0dad84a74
Jeff Passetti <jpassetti@Jeffs-MacBook-Air.local>
2023-02-13 13:20:46 -0500

    new registration form

--- pages/register.tsx
+++ pages/register.tsx
@@ -13,7 +13,7 @@ const RegisterPage = () => {
                 <Col xs={12} sm={12} paddingLeft={3}>
                     <Heading level={1} color="white" marginTop={8} marginBottom={3}>Register</Heading>
                     <Paragraph marginBottom={4}>To register for the workshop, please fill out the following form.</Paragraph>
-                    <iframe src="https://docs.google.com/forms/d/e/1FAIpQLSfEuhaZG-DacON-9fQQqjpd4WqJw24gfHfx_XZIdzB6-KECGA/viewform?c=0&w=1" width="100%" height="1310"></iframe>
+                    <iframe src="https://docs.google.com/forms/d/e/1FAIpQLSebzjcdleEwPu3YItR0jGVNPMIfuy40rsnGGK_zcgFcG-jJrA/viewform?c=0&w=1" width="100%" height="1310"></iframe>
                 </Col>
             </Row>
         </main>
